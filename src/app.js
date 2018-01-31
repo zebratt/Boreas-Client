c6bb343f2f3ebdcc1535ab59c1bd523f82dcde29
xuxuejian <jearbilove@yeah.net>
2018-01-31 16:44:54 +0800

    add canvas

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function initCanvas() {
+    const domCanvas = document.createElement('canvas')
+    domCanvas.classList.add('canvas')
+    domCanvas.setAttribute('height', innerHeight - 100)
+    domCanvas.setAttribute('width', document.documentElement.offsetWidth - 100)
+
+    document.body.appendChild(domCanvas)
+}
+
+//Entry
+initCanvas()
